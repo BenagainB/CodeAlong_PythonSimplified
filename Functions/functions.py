6d6775e9470dc28370ebb2e5811e92fe228bdcdc
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-05 07:58:46 -0700

    addressed pylance suggestions

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,21 +1,24 @@
 import random
 from sty import fg
 
-def generateRGB():
-    red = random.randint(0, 256)
-    green = random.randint(0, 256)
-    blue = random.randint(0, 256)
-    return red, green, blue
+def generate_RGB():
+    """generates random colors"""
+    red_int = random.randint(0, 256)
+    green_int = random.randint(0, 256)
+    blue_int = random.randint(0, 256)
+    return red_int, green_int, blue_int
 
-def generateColor(red, green, blue):
-    return fg(red, green, blue)
+def generate_color(red_value, green_value, blue_value):
+    """generate_color docstring"""
+    return fg(red_value, green_value, blue_value)
 
-red, green, blue = generateRGB()
-color = generateColor(red, green, blue)
+red, green, blue = generate_RGB()
+color = generate_color(red, green, blue)
 print(color, "I\'m randomly changing colors!\n")
 
 
 def about_me(name, profession, pet):
+    """prints biographical data"""
     print("Hi! My name is", name)
     print("I am a", profession)
     print("and I have a", pet, "\n")
