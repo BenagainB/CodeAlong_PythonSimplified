c48312babfd2b808253e55928d516b8fa144349e
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-04 21:03:34 -0700

    updated functions.py

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,8 +1,24 @@
+import random
+from sty import fg
+
+def generateRGB():
+    red = random.randint(0, 256)
+    green = random.randint(0, 256)
+    blue = random.randint(0, 256)
+    return red, green, blue
+
+def generateColor(red, green, blue):
+    return fg(red, green, blue)
+
+red, green, blue = generateRGB()
+color = generateColor(red, green, blue)
+print(color, "I\'m randomly changing colors!\n")
 
 
 def about_me(name, profession, pet):
     print("Hi! My name is", name)
     print("I am a", profession)
-    print("and I have a ", pet)
+    print("and I have a", pet, "\n")
 
 about_me("Ben", "programmer", "cat")
+about_me("Gandalf", "wizard", "mighty eagle")
