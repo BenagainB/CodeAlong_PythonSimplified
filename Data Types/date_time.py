c459fd99e9eb26949b05c086e2e702ce223fd2d3
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-07 23:04:07 -0700

    updated date_time.py

--- Data Types/date_time.py
+++ Data Types/date_time.py
@@ -1,6 +1,7 @@
 # date_time.py
 
 from datetime import date
+from datetime import datetime
 
 today = date.today()
 print("today is:", today)   # today is: 2023-04-07
@@ -19,3 +20,14 @@ print(next_year)    # 2024-04-07
 
 difference = abs(next_year - today)
 print("only {} days until next year".format(difference.days))
+    # only 366 days until next year
+
+# NikolaTesla = date(1856, 7, 10)   # alternate to following line
+NikolaTesla = date.fromisoformat("1856-07-10")
+print("Nickola Tesla was born on", NikolaTesla)
+    # Nickola Tesla was born on 1856-07-10
+print(NikolaTesla.weekday())    # 3 [which represents Thursday as where 0 = Monday and 6 = Sunday]
+
+now = datetime.now()
+print("right now it's:", now)   # right now it's: 2023-04-07 23:03:18.915554
+
