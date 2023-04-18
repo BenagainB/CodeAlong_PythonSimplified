0b56a401fa8a30c4b4efe9888fc69132f70cfb91
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-17 23:22:05 -0700

    started pandas.py

--- Libraries/pandas_intro.py
+++ Libraries/pandas_intro.py
@@ -1,1 +1,21 @@
 import pandas as pd
+
+column = ["Mariya", "Batman", "Spongebob"]
+# print(column)
+# ['Mariya', 'Batman', 'Spongebob']
+
+# data = pd.DataFrame(column)
+# print(data)
+#            0
+# 0     Mariya
+# 1     Batman
+# 2  Spongebob
+
+titled_column = {"name": column}
+data = pd.DataFrame(titled_column)
+print(data)
+#         name
+# 0     Mariya
+# 1     Batman
+# 2  Spongebob
+
