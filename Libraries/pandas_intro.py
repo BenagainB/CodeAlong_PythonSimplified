814f76f80ea536f0dd17c8a4bf2a61bf68115136
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-17 23:30:58 -0700

    added selecting row in dataframe

--- Libraries/pandas_intro.py
+++ Libraries/pandas_intro.py
@@ -11,11 +11,43 @@ column = ["Mariya", "Batman", "Spongebob"]
 # 1     Batman
 # 2  Spongebob
 
-titled_column = {"name": column}
-data = pd.DataFrame(titled_column)
-print(data)
+# titled_column = {"name": column}
+# data = pd.DataFrame(titled_column)
+# print(data)
 #         name
 # 0     Mariya
 # 1     Batman
 # 2  Spongebob
 
+titled_columns = {"name": column,
+                  "height": [1.67, 1.9, 0.25],
+                  "weight": [54, 100, 10]}
+
+data = pd.DataFrame(titled_columns)
+# print(data)
+#         name  height  weight
+# 0     Mariya    1.67      54
+# 1     Batman    1.90     100
+# 2  Spongebob    0.25      10
+
+# select_column = data["weight"]
+# print(select_column)
+# 0     54
+# 1    100
+# 2     10
+# Name: weight, dtype: int64
+
+select_column = data["weight"][1]
+# print(select_column)
+# 100
+
+# select_row = data.iloc[1]
+# print(select_row)
+# name      Batman
+# height       1.9
+# weight       100
+# Name: 1, dtype: object
+
+select_row = data.iloc[1]["weight"]
+print(select_row)
+# 100
