086edeb3c68e1e1166056b4a4cdd949ed316a733
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-19 09:57:09 -0700

    added printing rows from db

--- Libraries/sqlite.py
+++ Libraries/sqlite.py
@@ -1,6 +1,13 @@
 # sqlite.py
 """ SQLite Backend for Beginners """
+import os
 import sqlite3
+
+# Have to remove prior existing file gta.db or it interferes with loading/saving
+path = 'gta.db'
+if os.path.isfile(path):
+    os.remove(path)
+
 
 ## Create SQLite DB
 connection = sqlite3.connect("gta.db")
@@ -16,11 +23,18 @@ release_list = [
 
 # connection.close()
 
+
 ## SQL Cursor Object
 cursor = connection.cursor()
+
 
 ## Insert rows of data into a table
 cursor.execute("create table gta (release_year integer, release_name text, city text)")
 cursor.executemany("insert into gta values (?,?,?)", release_list)
+
+
+## Select and print database rows
+for row in cursor.execute("select * from gta"):
+    print(row)
 
 connection.close()
