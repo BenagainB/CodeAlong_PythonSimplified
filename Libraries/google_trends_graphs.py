37f7f3197be2305a1d37ba192aa375782bfe09cc
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-22 00:05:24 -0700

    extracted country data to dataframe

--- Libraries/google_trends_graphs.py
+++ Libraries/google_trends_graphs.py
@@ -37,15 +37,15 @@ data = pytrends.interest_over_time()
 # <class 'pandas.core.frame.DataFrame'>
 
 # plot data
-#plt.plot(data)
+# plt.plot(data)
 
 # add titles
-plt.suptitle('Programming Language Searches on Google')
-plt.xlabel('years')
-plt.ylabel('weekly searches')
+# plt.suptitle('Programming Language Searches on Google')
+# plt.xlabel('years')
+# plt.ylabel('weekly searches')
 
 # add legend
-plt.legend(keywords, loc='upper left')
+# plt.legend(keywords, loc='upper left')
 
 # save graph
 # plt.savefig('google_trends_graph.png')
@@ -53,9 +53,50 @@ plt.legend(keywords, loc='upper left')
 # plt.show()
 
 # analyze data
-focus = ['Python', 'R'] # refine search to 2 keywords
+# focus = ['Python', 'Java'] # refine search to 2 keywords
 
-plt.plot(data[focus])
-plt.legend(focus)
-# plt.savefig('google_trends_Python_vs_R.png')
+# plt.plot(data[focus])
+# plt.legend(focus)
+# plt.savefig('google_trends_Python_vs_Java.png')
 plt.show()
+
+# extract country-level data about the keywords
+data2 = pytrends.interest_by_region(resolution='COUNTRY', inc_low_vol=True)
+
+# get countries with the most searches of 'Python' over the years
+data2 = data2['Python'].nlargest(10)
+# print(data2)
+# geoName
+# China          50
+# Eritrea        44
+# Israel         41
+# St. Helena     32
+# Russia         28
+# Singapore      28
+# South Korea    28
+# Kyrgyzstan     26
+# Armenia        24
+# Hong Kong      24
+# Name: Python, dtype: int64
+
+# print(type(data2))
+# <class 'pandas.core.series.Series'>
+
+# convert to dataframe
+data2 = data2.to_frame()
+# print(data2)
+#              Python
+# geoName            
+# China            50
+# Eritrea          44
+# Israel           41
+# St. Helena       32
+# Russia           28
+# Singapore        28
+# South Korea      28
+# Kyrgyzstan       26
+# Armenia          24
+# Hong Kong        24
+
+# print(type(data2))
+# <class 'pandas.core.frame.DataFrame'>
