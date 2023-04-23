49050e2fc152a658a5751a66886ec48dafc7ebea
BenagainB <91215159+BenagainB@users.noreply.github.com>
2023-04-22 23:54:54 -0700

    update app_gui files

--- Libraries/app_gui.py
+++ Libraries/app_gui.py
@@ -1,2 +1,19 @@
 from dearpygui.core import *
 from dearpygui.simple import *
+
+# window object settings
+set_main_window_size(540, 720)
+set_global_font_scale(1.25)
+set_theme("Gold")
+set_style_window_padding(30, 30)
+
+with window("Simple SMS Spam Filter", width=520, height=677):
+    print("GUI is running...")
+    set_window_pos("Simple SMS Spam Filter", 0, 0)
+    add_drawing("logo", width=520, height=290)
+    add_separator()
+    add_spacing(count=12)
+
+draw_image("logo", "logo_spamFilter.png", [0,240])
+
+start_dearpygui()
